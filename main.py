177aa611cc92a6b091c024423f60e04de942629a
andre-novais <andrenovais.git@gmail.com>
2020-07-09 22:28:24 -0300

    agora tem importacao meeu

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+#from flask import Flask
+import sys
+import os
+from config import router
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, APP_ROOT)
+#import router
+app = router.rouuter()
 
 if __name__ == '__main__':
+    print("hey")
     app.run(host='0.0.0.0')
